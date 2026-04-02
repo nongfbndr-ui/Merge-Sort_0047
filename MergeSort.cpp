5d088880e9dc848f84f104538b9b8f3d1e52fc0a
Moreno <nongfbndr@gmail.com>
2026-04-02 17:39:10 +0700

    buat mergersort dasar

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -24,12 +24,53 @@ void input()
     }
 
     cout << "\n-------------------------------" << endl;
-    cout << "\nInputkan Isi element array" << endl;
-    cout << "-------------------------------" << endl;
-
-    for (int i = 0; i < n; i++)
+for (int i = 0; i < n; i++)
     {
         cout << "Data ke-" << (i + 1) << ": ";
         cin >> arr[i];
+    }    cout << "\nInputkan Isi element array" << endl;
+    cout << "-------------------------------" << endl;
+
+    
+}
+
+void merge(int low, int mid, int high)
+{
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = B[i];
     }
 }
