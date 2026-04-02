d7b4ec03da67eac5632d1bb5bf52224c08bb28c7
Moreno <nongfbndr@gmail.com>
2026-04-02 17:22:03 +0700

    input jumlah data

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,3 +6,30 @@ int main() {
 }
 int arr[20], B[20];
 int n;
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n-------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
